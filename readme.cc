0491af1fec3859ea124e51905c4b315c89b0889e
ZiJiang Bai <yqbyt0000@outlook.com>
2020-03-19 23:05:20 +0800

    Update readme.cc

--- readme.cc
+++ readme.cc
@@ -1,11 +1,1 @@
-/* Now,it was closed by no money! Following is only a simple statement.*/
-/* powered by hexo! */
-#include <bits/stdc++.h>
-
-using namespace std;
-
-int main (void)
-{
-    cout << “gu gu gu” << endl;
-    return 0;
-}
+It will start in August this year(2020).
